64f683b8cdbdc927b29dbe1ac694b71b12f0d00a
SaveliiYam <98161580+SaveliiYam@users.noreply.github.com>
2023-02-06 12:20:45 +0300

    Add files via upload

--- TanRegulator/TanRegulator.ino
+++ TanRegulator/TanRegulator.ino
@@ -1,14 +1,8 @@
-#include "7segments.h"
-#include "MyLib.h"
+#include <Segmentor.h>
 #include "Buttons.h"
 #include <GyverTimers.h>
 #include <GyverDimmer.h>
-Segmentor segment(5, 6, 7, 8, 9, 10, 11);
-unsigned long currentDisplay = 0;
-
-MyRelayOut firstTrans(A7);
-MyRelayOut secondTrans(3);
-MyRelayOut thirdTrans(4);
+Segmentor segment(7, 5, 9, 10, 11, 6, 8);
 
 Button buttonUp(A0);
 Button buttonDown(A1);
@@ -20,15 +14,17 @@ int valuePercent = 0;
 void setup() {
   attachInterrupt(0, isr, RISING);
   Timer2.enableISR();
+  segment.discharge(4, 3, 13);
 }
 
 void loop() {
   if(buttonUp.click()){valuePercent++;}
   if(buttonDown.click()){valuePercent--;}
-  if(valuePercent < 0){valuePercent = 0;}
-  else if(valuePercent > 100){valuePercent = 100;}
-  Display(valuePercent);
+  if(valuePercent <= 0){valuePercent = 0;}
+  else if(valuePercent >= 100){valuePercent = 100;}
+  segment.displayThree(valuePercent);
   dim.write(map(valuePercent, 0, 100, 0, 255));
+  //dim.write(100);
 }
 
 void isr() {
@@ -43,22 +39,4 @@ void isr() {
 ISR(TIMER2_A) {
   dim.tickTimer();    // вызвать tickTimer()
   Timer2.stop();      // останавливаем таймер
-}
-
-void Display(int value){
-  if(millis() - currentDisplay >= 10){
-    currentDisplay = millis();
-    int firstNumber = value%10;
-    int secondNumber = value%100/10;
-    int thirdNumber = value/100;
-    firstTrans.on();
-    segment.chooseNumber(firstNumber);
-    firstTrans.off();
-    secondTrans.on();
-    segment.chooseNumber(secondNumber);
-    secondTrans.off();
-    thirdTrans.on();
-    segment.chooseNumber(thirdNumber);
-    thirdTrans.off();
-  }
 }
